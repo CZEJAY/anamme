92c1238a7bdad5075b1c80e8f885e6106fdf1ada
CZEJAY <calebjimmy67@gmail.com>
2024-03-18 11:39:04 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,25 +5,30 @@ import Link from "next/link";
 
 export default async function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-between gap-6 border-t border-slate-600 px-8 py-7 md:flex-row">
-      <Link href="/">
-        <WordMark />
-        <span className="sr-only">Anamme Home Page</span>
-      </Link>
-      <div className="text-xs flex flex-col lg:flex-row items-center">
-        Copyright &copy; 2006 - 2024 Anamme Nigeria Enterpise | <br /> <span className="ml-1 text-center">  All Rights Reserved</span>
-      </div>
-      <nav aria-label="Footer">
-        <div className="flex gap-6">
-          {navbarLinks.map((item) => {
-            return (
-              <Link key={item.title} href={item.link}>
-                {item.title}
-              </Link>
-            );
-          })}
+    <>
+      <div className="mx-auto text-3xl font-bold p-12">Join Our Satisfied Clients​ Today!</div>
+
+      <footer className="flex flex-col items-center justify-between gap-6 border-t border-slate-600 px-8 py-7 md:flex-row">
+        <Link href="/">
+          <WordMark />
+          <span className="sr-only">Anamme Home Page</span>
+        </Link>
+        <div className="text-xs flex flex-col lg:flex-row items-center">
+          Copyright &copy; 2006 - 2024 Anamme Nigeria Enterpise | <br />{" "}
+          <span className="ml-1 text-center"> All Rights Reserved</span>
         </div>
-      </nav>
-    </footer>
+        <nav aria-label="Footer">
+          <div className="flex gap-6">
+            {navbarLinks.map((item) => {
+              return (
+                <Link key={item.title} href={item.link}>
+                  {item.title}
+                </Link>
+              );
+            })}
+          </div>
+        </nav>
+      </footer>
+    </>
   );
 }
